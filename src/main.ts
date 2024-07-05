bbb8c934c516ed33f6b4e2b73fdb50a623b4c1d9
websorokinweb <btrubiuk@gmail.com>
2024-07-05 19:37:40 +0200

    main hero first position

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,34 @@
-import { Application } from 'pixi.js'
+import { Application, Graphics, Rectangle } from 'pixi.js'
 ;(async () => {
 	const app = new Application()
 	await app.init({
 		background: '#3B5D17',
-    resizeTo: window
+		resizeTo: window,
 	})
 
 	globalThis.__PIXI_APP__ = app
 
 	document.body.appendChild(app.canvas)
 
-	const link = document.createElement('link')
+  addStyles()
+
+  const appWidth = app.screen.width
+  const appHeight = app.screen.height
+	
+
+	function addMainHero() {
+		const mainHero = new Graphics().circle(appWidth / 2, appHeight / 2, 50).fill(0xff0000)
+
+		app.stage.addChild(mainHero)
+	}
+
+	addMainHero()
+})()
+
+
+function addStyles(): void{
+  const link = document.createElement('link')
 	link.rel = 'stylesheet'
 	link.href = 'src/style.css'
 	document.head.appendChild(link)
-})()
+}
