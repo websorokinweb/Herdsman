2d675cbe1e4eccaae1d12586c6ff1ab327dd42ac
websorokinweb <btrubiuk@gmail.com>
2024-07-05 20:12:52 +0200

    move main hero without animation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import { Application, Graphics, Rectangle } from 'pixi.js'
+import { Application, Graphics } from 'pixi.js'
 ;(async () => {
 	const app = new Application()
 	await app.init({
-		background: '#3B5D17',
 		resizeTo: window,
 	})
 
@@ -10,24 +9,56 @@ import { Application, Graphics, Rectangle } from 'pixi.js'
 
 	document.body.appendChild(app.canvas)
 
-  addStyles()
+	addStyles()
 
-  const appWidth = app.screen.width
-  const appHeight = app.screen.height
-	
+	const appWidth = app.screen.width
+	const appHeight = app.screen.height
 
-	function addMainHero() {
-		const mainHero = new Graphics().circle(appWidth / 2, appHeight / 2, 50).fill(0xff0000)
+	let gameField: Graphics | null = null
+	let mainHero: Graphics | null = null
 
-		app.stage.addChild(mainHero)
+	function addGameField(): Graphics {
+		const gameField = new Graphics()
+			.rect(0, 0, appWidth, appHeight)
+			.fill('#3B5D17')
+
+		app.stage.addChild(gameField)
+
+		gameField.on('pointerdown', (pointerDownEvent) => {
+			console.log({ pointerDownEvent })
+			moveMainHero(pointerDownEvent.x, pointerDownEvent.y)
+		})
+		gameField.eventMode = 'static'
+
+		return gameField
 	}
 
-	addMainHero()
+	gameField = addGameField()
+
+	function addMainHero(): Graphics {
+		const mainHero = new Graphics().circle(0, 0, 50).fill(0xff0000)
+
+		mainHero?.position.set(appWidth / 2, appHeight / 2)
+
+		app.stage.addChild(mainHero)
+
+		mainHero.on('pointerdown', (pointerDownEvent) => {
+			console.log({ pointerDownEvent })
+		})
+		mainHero.eventMode = 'static'
+
+		return mainHero
+	}
+
+	function moveMainHero(x: number, y: number) {
+		mainHero?.position.set(x, y)
+	}
+
+	mainHero = addMainHero()
 })()
 
-
-function addStyles(): void{
-  const link = document.createElement('link')
+function addStyles(): void {
+	const link = document.createElement('link')
 	link.rel = 'stylesheet'
 	link.href = 'src/style.css'
 	document.head.appendChild(link)
