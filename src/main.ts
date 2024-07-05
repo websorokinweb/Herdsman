661715908736d938f3d04adec68f32e93b95371c
websorokinweb <btrubiuk@gmail.com>
2024-07-05 19:15:26 +0200

    fullscreen canvas

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 import { Application } from 'pixi.js'
-
 ;(async () => {
 	const app = new Application()
 	await app.init({
-		width: 640,
-		height: 360,
+		width: 600,
+		height: 600,
 	})
 
 	globalThis.__PIXI_APP__ = app
 
 	document.body.appendChild(app.canvas)
+
+	const link = document.createElement('link')
+	link.rel = 'stylesheet'
+	link.href = 'src/style.css'
+	document.head.appendChild(link)
+
+	window.addEventListener('resize', onResize)
+
+	function onResize() {
+		app.renderer.resize(window.innerWidth, window.innerHeight)
+	}
+
+	onResize()
 })()
