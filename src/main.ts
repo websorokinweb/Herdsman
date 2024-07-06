dd6dd5a0d2ff33849f09ded7018010d26af000e4
websorokinweb <btrubiuk@gmail.com>
2024-07-06 11:04:42 +0200

    destination field + display counter

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Application, Graphics } from 'pixi.js'
+import { Application, Graphics, Text } from 'pixi.js'
 ;(async () => {
 	const app = new Application()
 	await app.init({
@@ -14,11 +14,16 @@ import { Application, Graphics } from 'pixi.js'
 	const appWidth = app.screen.width
 	const appHeight = app.screen.height
 
+  app.stage.sortableChildren = true
+
 	let gameField: Graphics | null = null
 	let mainHero: Graphics | null = null
+	let destinationField: Graphics | null = null
+
+  let mainHeroScoreCounter: Text | null = null
 
 	function addGameField(): Graphics {
-		const gameField = new Graphics()
+		const gameField: Graphics = new Graphics()
 			.rect(0, 0, appWidth, appHeight)
 			.fill('#3B5D17')
 
@@ -33,19 +38,51 @@ import { Application, Graphics } from 'pixi.js'
 		return gameField
 	}
 
+  // console.log(app.ticker.deltaTime)
+  // console.log(app.ticker.minFPS)
+  // console.log(app.ticker.maxFPS)
+  // console.log(app.ticker.FPS)
+
 	gameField = addGameField()
 
+  function addMainHeroScoreCounter(): Text {
+    const mainHeroScoreCounter: Text = new Text({
+      text: '0',
+      style: {
+        fill: 0xFFFFFF,
+        fontSize: 24
+      }
+    })
+
+    mainHeroScoreCounter.position.set(10, 10)
+
+    mainHeroScoreCounter.zIndex = 9999
+
+    app.stage.addChild(mainHeroScoreCounter)
+
+    return mainHeroScoreCounter
+  }
+
+  mainHeroScoreCounter = addMainHeroScoreCounter()
+
+  function addDestinationField(): Graphics {
+    const destinationField: Graphics = new Graphics().rect(0, 0, 300, 500).fill(0xF0E68C)
+
+    destinationField.position.set(appWidth - 300, (appHeight / 2) - destinationField.height / 2)
+
+    app.stage.addChild(destinationField)
+
+    return destinationField
+  }
+
+  destinationField = addDestinationField()
+
 	function addMainHero(): Graphics {
-		const mainHero = new Graphics().circle(0, 0, 50).fill(0xff0000)
+		const mainHero: Graphics = new Graphics().circle(0, 0, 25).fill(0xff0000)
 
 		mainHero?.position.set(appWidth / 2, appHeight / 2)
 
 		app.stage.addChild(mainHero)
-
-		mainHero.on('pointerdown', (pointerDownEvent) => {
-			console.log({ pointerDownEvent })
-		})
-		mainHero.eventMode = 'static'
 
 		return mainHero
 	}
